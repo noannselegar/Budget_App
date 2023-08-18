91c3aa9c8a0fde5d4206766742d1133ef4f59d80
Noann Selegar <noannselegar@gmail.com>
2023-08-18 16:18:24 -0300

    initial commit

---
+++ budget.py
@@ -0,0 +1,52 @@
+class Category:
+    def __init__(self, name):
+        self.name = name
+        self.ledger = list()
+        self.funds = 0
+
+    def check_funds(self, amount):
+        if amount <= self.funds: return True
+        else: return False
+    
+    def deposit(self, amount, description=''):
+        self.funds += amount
+        self.ledger.append({'amount':amount, 'description':description})
+
+
+    def withdraw(self, amount, description=''):
+        if self.check_funds(amount):
+            self.funds -= amount
+            self.ledger.append({'amount':-amount, 'description':description})
+            return True
+        else:
+            print('Not sufficient funds')
+            return False
+        
+    def get_balance(self):
+        print(self.funds)
+        return self.funds
+    
+    def transfer(self, amount, varname):
+        if self.check_funds(amount):
+            self.funds -= amount
+            self.ledger.append({'amount':-amount, 'description':f'Transfer to {varname.name}'})
+            varname.funds += amount
+            varname.ledger.append({'amount':amount, 'description':f'Transfer from {self.name}'})
+            return True
+        else:
+            print('Not sufficient funds')
+            return False
+    
+    def list(self):
+        print(self.name.center(30, '*'))
+        for pair in self.ledger:
+            print(f'{pair["description"].ljust(20)}{str(pair["amount"]).rjust(10)}', end='')
+            print('')
+        print('-'.center(30, '-'))
+        print('Total: ', float(self.funds))
+        print('\n')
+
+def spend_chart(cat_names):
+    amounts = [p['amount'] for name in cat_names for p in name.ledger if p['amount'] < 0]
+    spent = {name.name:p['amount'] for name in cat_names for p in name.ledger if p['amount'] < 0}
+    print(spent, amounts)
---
+++ main.py
@@ -0,0 +1,20 @@
+from budget import *
+
+food = Category('FOOD')
+shopping = Category('SHOPPING')
+clothing = Category('CLOTHING')
+
+clothing.deposit(100, 'initial deposit')
+shopping.deposit(300, 'initial deposit')
+food.deposit(600, 'initial deposit')
+
+food.withdraw(10.50)
+shopping.withdraw(11.50)
+food.withdraw(5.50)
+shopping.withdraw(70)
+clothing.withdraw(99)
+#food.list()
+#shopping.list()
+#clothing.list()
+
+spend_chart([food, shopping, clothing])
